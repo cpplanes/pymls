3c970281103f7aa43dcc2bceb6241f0dec45b95b
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2017-08-30 18:46:03 +0200

    Add fluid medium

---
+++ pypw/media/fluid.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding:utf8 -*-
+#
+# fluid.py
+#
+# This file is part of pypw, a software distributed under the MIT license.
+# For any question, please contact one of the authors cited below.
+#
+# Copyright (c) 2017
+# 	Olivier Dazel <olivier.dazel@univ-lemans.fr>
+# 	Mathieu Gaborit <gaborit@kth.se>
+# 	Peter Göransson <pege@kth.se>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+
+from .medium import Medium
+
+
+class Elastic(Medium):
+
+    MEDIUM_TYPE = 'fluid'
+    EXPECTED_FIELDS = [
+        'rho',  # Density
+        'c'  # Sound speed
+    ]
+
+    def __init__(self):
+        super().__init__()
+
+        self.rho = None
+        self.c = None
+
+    def update_frequency(self, frequency):
+        self.frequency = frequency
