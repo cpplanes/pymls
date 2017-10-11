5a5062b0cc824d1d58fb8dea0d322e36737e0a5c
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2017-10-11 16:00:49 +0200

    Add a lot of tests to the testbase

--- test/solver.py
+++ test/solver.py
@@ -23,14 +23,13 @@
 #
 
 import unittest
-import tempfile
 import os
 import itertools
 
 import numpy as np
 
 from pymls import Solver, Layer, backing, from_yaml
-from pymls.media import Air
+from pymls.media import Air, EqFluidJCA
 
 THIS_FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -44,7 +43,6 @@ NB_PLACES = 10
 
 
 class SolverTests(unittest.TestCase):
-
 
     def test_air_analytical(self):
 
@@ -78,7 +76,6 @@ class SolverTests(unittest.TestCase):
                     reference = np.loadtxt(THIS_FILE_DIR+f'/references/{mat_name}_{int(d*1e3)}mm_{backing_type}.csv')
 
                     for l in reference:
-                        # print('\n');print(l[0]);print(l[1]);print(d);print(backing_type)
                         S = Solver()
                         S.layers = [Layer(mat, d)]
                         S.backing = backing_func
@@ -87,12 +84,6 @@ class SolverTests(unittest.TestCase):
                         self.assertAlmostEqual(result['R'][0], l[2]+1j*l[3], tol)
                         if backing_func == backing.transmission:
                             self.assertAlmostEqual(result['T'][0], l[4]+1j*l[5], tol)
-
-    # def test_elastic_numerical(self):
-    #     self.helper_numerical_tests(['wood', 'glass'], BACKINGS, NB_PLACES)
-    #
-    # def test_pem_numerical(self):
-    #     self.helper_numerical_tests(['foam2'], BACKINGS, NB_PLACES)
 
     def test_elastic_rigid_numerical(self):
         self.helper_numerical_tests(['wood', 'glass'], BACKINGS[:0], NB_PLACES)
@@ -122,7 +113,6 @@ class SolverTests(unittest.TestCase):
                 reference = np.loadtxt(filename)
 
                 for l in reference:
-                    # print('\n');print(l[0]);print(l[1]);print(d);print(backing_type)
                     S = Solver()
                     S.layers = [Layer(mat1, d1), Layer(mat2, d2)]
                     S.backing = backing_func
@@ -143,3 +133,101 @@ class SolverTests(unittest.TestCase):
 
     def test_2xpem_transmission_numerical(self):
         self.helper_bi_mat('foam2', 'foam2', BACKINGS[1:1], 10)
+
+    def helper_numerical_tests_eqf(self, materials, backings, tol):
+
+        for mat_name in materials:
+            mat = from_yaml(THIS_FILE_DIR+f'/materials/{mat_name}.yaml', force=EqFluidJCA)
+
+            for (backing_type, backing_func) in backings:
+                for d in THICKNESSES:
+                    for a in ANGLES:
+                        reference = np.loadtxt(THIS_FILE_DIR+f'/references/eqf/{mat_name}_{int(d*1e3)}mm_{backing_type}_{a}deg.PW')
+
+                        for l in reference:
+                            S = Solver()
+                            S.layers = [Layer(mat, d)]
+                            S.backing = backing_func
+                            result = S.solve(l[0], a)
+
+                            self.assertAlmostEqual(
+                                result['R'][0], l[2]+1j*l[3], tol,
+                                f'(reflection) {mat_name} : f={l[0]}Hz angle={a}deg d={d:2.3f}m'
+                            )
+                            if backing_func == backing.transmission:
+                                self.assertAlmostEqual(
+                                    result['T'][0], l[5]+1j*l[6], tol,
+                                    f'(transmission) {mat_name} : f={l[0]}Hz angle={a}deg d={d:2.3f}m'
+                                )
+
+    def helper_bi_mat_eqf(self, mat1_name, mat2_name, backings, tol, no_is_default=-1, ref_path='eqf'):
+
+        if no_is_default == 1:
+            mat1 = from_yaml(THIS_FILE_DIR+f'/materials/{mat1_name}.yaml')
+        else:
+            mat1 = from_yaml(THIS_FILE_DIR+f'/materials/{mat1_name}.yaml', force=EqFluidJCA)
+
+        if no_is_default == 2:
+            mat2 = from_yaml(THIS_FILE_DIR+f'/materials/{mat2_name}.yaml')
+        else:
+            mat2 = from_yaml(THIS_FILE_DIR+f'/materials/{mat2_name}.yaml', force=EqFluidJCA)
+
+        prefix = f'{mat1_name}_{mat2_name}'
+
+        for (backing_type, backing_func) in backings:
+            for (d1, d2) in itertools.product(THICKNESSES, THICKNESSES):
+                for a in ANGLES:
+                    filename = THIS_FILE_DIR+f'/references/{ref_path}/{prefix}_{int(d1*1e3)}mm_{int(d2*1e3)}mm_{backing_type}_{a}deg.PW'
+                    if not os.path.exists(filename):
+                        continue
+
+                    reference = np.loadtxt(filename)
+
+                    for l in reference:
+                        S = Solver()
+                        S.layers = [Layer(mat1, d1), Layer(mat2, d2)]
+                        S.backing = backing_func
+                        result = S.solve(l[0], a)
+
+                        self.assertAlmostEqual(
+                            result['R'][0], l[2]+1j*l[3], tol,
+                            f'(reflection) {mat1_name} {mat2_name} : f={l[0]}Hz angle={a}deg d1={d1:2.3f}m d2={d2:2.3f}m'
+                        )
+                        if backing_func == backing.transmission:
+                            self.assertAlmostEqual(
+                                result['T'][0], l[5]+1j*l[6], tol,
+                                f'(transmission) {mat1_name} {mat2_name} : f={l[0]}Hz angle={a}deg d1={d1:2.3f}m d2={d2:2.3f}m'
+                            )
+
+    def test_foams_eqf(self):
+        self.helper_numerical_tests_eqf(['foam', 'foam2'], BACKINGS, NB_PLACES)
+
+    # def test_foam_wood_eqf(self):
+    #     self.helper_bi_mat_eqf('foam', 'wood', BACKINGS, NB_PLACES, no_is_default=2)
+    #     self.helper_bi_mat_eqf('wood', 'foam', BACKINGS, NB_PLACES, no_is_default=1)
+    #
+    # def test_foam2_wood_eqf(self):
+    #     self.helper_bi_mat_eqf('foam2', 'wood', BACKINGS, NB_PLACES, no_is_default=2)
+    #     self.helper_bi_mat_eqf('wood', 'foam2', BACKINGS, NB_PLACES, no_is_default=1)
+    #
+    def test_foam_foam_eqf(self):
+        self.helper_bi_mat_eqf('foam', 'foam', BACKINGS, NB_PLACES)
+
+    def test_foam2_foam2_eqf(self):
+        self.helper_bi_mat_eqf('foam2', 'foam2', BACKINGS, NB_PLACES)
+
+    def test_foam2_foam_eqf(self):
+        self.helper_bi_mat_eqf('foam2', 'foam', BACKINGS, NB_PLACES)
+        self.helper_bi_mat_eqf('foam', 'foam2', BACKINGS, NB_PLACES)
+
+    def test_eqf_pem(self):
+        self.helper_bi_mat_eqf('foam', 'foam', BACKINGS, NB_PLACES, no_is_default=2, ref_path='eqf_pem')
+        self.helper_bi_mat_eqf('foam', 'foam2', BACKINGS, NB_PLACES, no_is_default=2, ref_path='eqf_pem')
+        self.helper_bi_mat_eqf('foam2', 'foam2', BACKINGS, NB_PLACES, no_is_default=2, ref_path='eqf_pem')
+        self.helper_bi_mat_eqf('foam2', 'foam', BACKINGS, NB_PLACES, no_is_default=2, ref_path='eqf_pem')
+
+    def test_pem_eqf(self):
+        self.helper_bi_mat_eqf('foam', 'foam', BACKINGS, NB_PLACES, no_is_default=1, ref_path='eqf_pem')
+        self.helper_bi_mat_eqf('foam', 'foam2', BACKINGS, NB_PLACES, no_is_default=1, ref_path='eqf_pem')
+        self.helper_bi_mat_eqf('foam2', 'foam2', BACKINGS, NB_PLACES, no_is_default=1, ref_path='eqf_pem')
+        self.helper_bi_mat_eqf('foam2', 'foam', BACKINGS, NB_PLACES, no_is_default=1, ref_path='eqf_pem')
